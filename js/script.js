d559b4c9a880d363df597bd4c94167b4167de460
Andreska <andreska_nayara@hotmail.com>
2023-10-19 11:17:42 -0300

    criação das funções de check

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("form");
-const name = document.querySelector("#name");
-const about = document.querySelector("#sobre");
+const nameInput = document.querySelector("#name");
+const aboutInput = document.querySelector("#sobre");
 const number = document.querySelector("#number");
 const instruction = document.querySelector("#instruction");
 const time = document.querySelector("#time");
@@ -9,5 +9,39 @@ const sendBtn = document.querySelector("#contact");
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    console.log(e);  
+    checkname();  
+    checkabout();
+    // checknumber();
+    // checkinstruction();
+    // checktime();
 });
+
+function checkname() {
+    const nameValue = nameInput.value;
+
+   console.log(nameValue);
+};
+
+function checkabout() {
+    const aboutInputValue = aboutInput.value;
+
+   console.log(aboutInputValue);
+};
+
+function checknumber() {
+    const numberValue = number.value;
+
+   console.log(numberValue);
+};
+
+function checkinstruction() {
+    const instructionValue = instruction.value;
+
+   console.log(instructionValue);
+};
+
+function checktime() {
+    const timeValue = time.value;
+
+   console.log(timeValue);
+};
